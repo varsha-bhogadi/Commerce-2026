7b96fb05f378ef04380495fe81f479b1fc2e4662
sbhogadi <sbhogadi@adobe.com>
2026-02-04 05:32:21 +0000

    initial commit

---
+++ examples/first-file.php
@@ -0,0 +1,14 @@
+<?php
+$n1 = 26;
+$n2 = 79;
+echo $n1+$n2;
+
+$name = "alex";
+$name2 = "bob";
+// to concatenate 2 strings use '.' operator
+echo $name . ", " . $name2;
+
+$name3 = 'alex';
+$name4 = 'bob';
+echo $name3 . ', ' . $name4;
+
---
+++ examples/string-methods.php
@@ -0,0 +1,16 @@
+<?php
+$greeting = 'good morning!';
+echo $greeting;
+echo substr($greeting, 4, 3);
+echo strlen($greeting);
+echo strtoupper($greeting);
+echo strtolower($greeting);
+echo trim($greeting);
+echo ucfirst($greeting);
+echo ucwords($greeting);
+echo str_shuffle($greeting);
+echo str_word_count($greeting);
+echo strrev($greeting);
+echo str_repeat($greeting,2);
+
+
